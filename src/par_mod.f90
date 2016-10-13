df4a68e29d6e1ee799517bb522153c88e449f181
Sabine <sabine.eckhardt@nilu.no>
2016-10-13 16:33:58 +0200

    20Mio particles

--- src/par_mod.f90
+++ src/par_mod.f90
@@ -185,7 +185,7 @@ module par_mod
   ! Maximum number of particles, species, and similar
   !**************************************************
 
-  integer,parameter :: maxpart=40000000
+  integer,parameter :: maxpart=20000000
   integer,parameter :: maxspec=2
   real,parameter :: minmass=0.0001
 
