6d420c3a4255da1d62e94bf47994527ba5bdd0ff
Ignacio Pisso <Ignacio.Pisso@nilu.no>
2017-05-17 00:19:46 +0200

    add create_tarball.sh to dev branch for release of version 10.2

---
+++ create_tarball.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version=10.2 
+tarball_tmp=flexpart$version 
+
+mkdir $tarball_tmp
+mkdir $tarball_tmp/src
+cp src/*.f90 $tarball_tmp/src
+cp src/makefile $tarball_tmp/src
+cp src/makefile.gfs $tarball_tmp/src
+cp -r options $tarball_tmp
+
+mkdir $tarball_tmp/tests
+cp -r tests/NILU/test_1 $tarball_tmp/tests/
+
+#return
+
+tar -cvf flexpart$version.tar $tarball_tmp/*
+
